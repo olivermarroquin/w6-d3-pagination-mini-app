912fe0a20f47d1838c897607eff31e20adb00712
omarroq2 <oliver.marroquin31217@gmail.com>
2026-03-11 21:29:11 -0400

    added some dom selections and page state

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+//TODO Grab the elements from the HTML so that I can update them later
+
+const statusEl = document.querySelector("#status");
+const pageInfoEl = document.querySelector("#page-info");
+const resultsEl = document.querySelector("#results");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+//TODO create state
+
+let currentPage = 1;
+let totalItems = 0;
+let totalPages = 1;
